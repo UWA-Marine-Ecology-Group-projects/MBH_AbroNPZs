7ae6db5af427a8fb7feb53b43738acf24d3ffee5
anitas-giraldo <anita.giraldo@merineecology.io>
2021-05-04 02:25:07 +0000

    commit

--- alter_inclusion_probs.R
+++ alter_inclusion_probs.R
@@ -31,6 +31,10 @@ legacyss <- legacys[,c(6,7)]
 # in ascending longitude
 legacyss <- arrange(legacyss, coords.x1)
 names(legacyss) <- c("x", "y")
+class(legacyss)
+
+# test with just one
+legacyss <- legacyss[c(3,8),]
 
 
 # Read inclusion probabilities ----
@@ -41,12 +45,16 @@ plot(inclProbs)
 
 # potential sites ----
 pot.sites <- as.data.frame(inclProbs, xy = TRUE)
+head(pot.sites)
+pot.sites <- arrange(pot.sites, y)
+head(pot.sites)
 class(pot.sitess)
 
 # as df
 pot.sitess <- pot.sites[,c(1,2)]
 
 # inclusion probs as numeric or df
+ip <- as.data.frame(pot.sites[,3])
 ip <- pot.sites[,3]
 class(ip)
 
@@ -66,7 +74,12 @@ plot(altInclProbs)
 
 #visualise
 image( x=unique( pot.sitess[,1]), y=unique( pot.sitess[,2]),
-       z=matrix( altInclProbs, nrow=sqrt(nrow(pot.sitess)), ncol=sqrt(nrow( pot.sitess))),
-       main="Adjusted Inclusion Probabilities",
+       z=matrix( ip, nrow=100, ncol=43),
+       main="Inclusion Probabilities (Undadjusted)",
        ylab=colnames( pot.sitess)[2], xlab=colnames( pot.sitess)[1])
+
+image( x=unique( X[,1]), y=unique( X[,2]),
+       z=matrix( inclProbs, nrow=sqrt(nrow(X)), ncol=sqrt(nrow( X))),
+       main="Adjusted Inclusion Probabilities",
+       ylab=colnames( X)[2], xlab=colnames( X)[1])
 
